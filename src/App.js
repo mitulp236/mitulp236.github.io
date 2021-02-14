98080fc236aa238004df61d237f66043ab467861
Mitul Patel <mitul.srashtasoft@gmail.com>
2021-02-14 14:43:15 +0530

    new Pipe

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. yes new config
+          Edit <code>src/App.js</code> and save to reload. Pipeline
         </p>
         <a
           className="App-link"
