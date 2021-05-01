2c8c0bd0d7160a7887880585b2db2232ed08af79
Mitul Patel <mitul.srashtasoft@gmail.com>
2021-05-02 02:18:26 +0530

    new

---
+++ _next/static/webpack/pages/index.cf9f801f70445a39c066.hot-update.js
@@ -0,0 +1,22 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./data/siteMetadata.json":
+/*!********************************!*\
+  !*** ./data/siteMetadata.json ***!
+  \********************************/
+/***/ (function(module) {
+
+"use strict";
+module.exports = JSON.parse('{"title":"The broken developer\'s portfolio","author":"Mitul Patel","headerTitle":"Devloper portfolio","description":"A web developer portfolio","language":"en-us","siteUrl":"https://tailwind-nextjs-starter-blog.vercel.app","siteRepo":"https://github.com/timlrx/tailwind-nextjs-starter-blog","image":"/static/images/mitul.png","socialBanner":"/static/images/twitter-card.png","email":"mitulp236@gmail.com","github":"https://github.com/mitulp236","twitter":"https://twitter.com/mitulp236","facebook":"https://facebook.com/mitulp236","instagram":"https://instagram.com/mitulp236","youtube":"https://www.youtube.com/channel/UCX-9JA9ydtbLgXgMZdP7ozg","linkedin":"https://www.linkedin.com/mitulp236","locale":"en-US","moboleno":"+91 8140850797"}');
+
+/***/ })
+
+});
