72c6ae1908da4f1e040ad5f5841e1cf4b24e5105
mitul Patel <mitulp236@gmail.com>
2021-05-02 17:17:30 +0530

    Deploy Next.js to gh-pages

---
+++ out/_next/static/Y8F1Ksjq9Ju5rjbyNrpG6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3ba2cd86b1183716e7ec.js"],"/_error":["static/chunks/pages/_error-8f84588b5c0c8a125c4e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
