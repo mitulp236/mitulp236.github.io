127685cf9752311daaff379097b61f108f23d185
Mitul Patel <mitulp236@gmail.com>
2021-05-02 18:26:47 +0530

    df

--- config.js
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? `http://localhost:1337` : `http://3fd0749a656b.ngrok.io`;
--- data/headerNavLinks.js
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-    { href: '/', title: 'About' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/contact', title: 'Contact' },
-  { href: '/projects', title: 'Projects' },
-
-]
-
-export default headerNavLinks
